29fd7da3e5c4bae0c8ca3681f99c5df4a1a25ef7
Nelber Mora <nelbermora@gmail.com>
2024-04-02 18:01:44 -0300

    isolating gemini service

--- gemini/gemini.go
+++ gemini/gemini.go
@@ -9,20 +9,6 @@ import (
 	"google.golang.org/api/option"
 )
 
-func Init() {
-	/*ctx := context.Background()
-	// Access your API key as an environment variable (see "Set up your API key" above)
-	var err error
-	Client, err = genai.NewClient(ctx, option.WithAPIKey("AIzaSyDxrBg5kH9z1-IVpbOd7CBZ0AwOzDRaJkw"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	//defer Client.Close()
-
-	Model = Client.GenerativeModel("gemini-1.0-pro")
-	*/
-}
-
 func AskForData(input, key string) ([]string, error) {
 	ctx := context.Background()
 	output := []string{}
--- gemini/gemini_test.go
+++ gemini/gemini_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestClient(t *testing.T) {
-	Init()
 	out, err := AskForData("Puedes generar preguntas y respuestas frecuentes del Producto de Nombre Ranger XLS de la marca Ford?", "")
 	log.Println(out)
 	assert.NotNil(t, err)
